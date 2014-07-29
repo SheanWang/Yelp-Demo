7f3fed528114385cae1b3ad2bd3c0ae7ef40591a
SheanWang <swang@microsoft.com>
2014-07-29 08:51:14 -0700

    aa

---
+++ Yelp/MainPage.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Yelp.Resources;
+
+namespace Yelp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        System.Windows.Threading.DispatcherTimer dt = new System.Windows.Threading.DispatcherTimer();
+
+        private struct AdInfo
+        {
+            public AdInfo(string image, string location, string phone, string title)
+            {
+                this.image = image;
+                this.location = location;
+                this.phone = phone;
+                this.title = title;
+            }
+
+            public string image;
+            public string location;
+            public string phone;
+            public string title;
+        }
+
+        AdInfo[] ads = new AdInfo[3]
+        {
+            new AdInfo("Assets/Ad1_Title.png", "1 km", "(202) 580-8889", "Mitchell's Fish Market"), 
+            new AdInfo("Assets/Ad2_Title.png", "777 m", "(248) 646-3663", "Restaurant Reservations"), 
+            new AdInfo("Assets/Ad3_Title.png", "1.5 km", "(010) 111222", "Rose's Luxury")
+        };
+
+        int i = 0;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            BuildAds();
+            
+            dt.Interval = new TimeSpan(0, 0, 0, 0, 5000); // 500 Milliseconds
+            dt.Tick += new EventHandler(dt_Tick);
+            dt.Start();
+        }
+
+        void dt_Tick(object sender, EventArgs e)
+        {
+
+            i %= 3;
+            Title1.Text = ads[i].title;
+            Phone1.Text = ads[i].phone;
+            Location1.Text = ads[i].location;
+            BitmapImage tn = new BitmapImage();
+            tn.SetSource(Application.GetResourceStream(new Uri(ads[i].image, UriKind.Relative)).Stream);
+            Image1.Source = tn;
+
+            Title1.Visibility = System.Windows.Visibility.Visible;
+            Phone1.Visibility = System.Windows.Visibility.Visible;
+            Delete1.Visibility = System.Windows.Visibility.Visible;
+            DeleteImage1.Visibility = System.Windows.Visibility.Visible;
+            Location1.Visibility = System.Windows.Visibility.Visible;
+            Image1.Visibility = System.Windows.Visibility.Visible;
+
+            i++;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page1.xaml", UriKind.Relative)); 
+        }
+
+        private void BuildAds()
+        {
+            Title1.Visibility = System.Windows.Visibility.Collapsed;
+            Phone1.Visibility = System.Windows.Visibility.Collapsed;
+            Delete1.Visibility = System.Windows.Visibility.Collapsed;
+            DeleteImage1.Visibility = System.Windows.Visibility.Collapsed;
+            Location1.Visibility = System.Windows.Visibility.Collapsed;
+            Image1.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Title1.Visibility = System.Windows.Visibility.Collapsed;
+            Phone1.Visibility = System.Windows.Visibility.Collapsed;
+            Delete1.Visibility = System.Windows.Visibility.Collapsed;
+            DeleteImage1.Visibility = System.Windows.Visibility.Collapsed;
+            Location1.Visibility = System.Windows.Visibility.Collapsed;
+            Image1.Visibility = System.Windows.Visibility.Collapsed;
+
+            dt.Stop();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page2.xaml", UriKind.Relative)); 
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
---
+++ Yelp/Page2.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Yelp
+{
+    public partial class Page2 : PhoneApplicationPage
+    {
+        System.Windows.Threading.DispatcherTimer dt = new System.Windows.Threading.DispatcherTimer();
+
+        private struct AdInfo
+        {
+            public AdInfo(string image, string location, string phone, string title)
+            {
+                this.image = image;
+                this.location = location;
+                this.phone = phone;
+                this.title = title;
+            }
+
+            public string image;
+            public string location;
+            public string phone;
+            public string title;
+        }
+
+        AdInfo[] ads = new AdInfo[3]
+        {
+            new AdInfo("Assets/Ad4_Title.png", "50 m", "+1 202-291-5000", "Red Derby"), 
+            new AdInfo("Assets/Ad5_Title.png", "990 m", "(202) 333-7700", "Gypsy Sally's"), 
+            new AdInfo("Assets/Ad6_Title.png", "3.9 km", "(202) 419-6755", "Blue Duck Tavern")
+        };
+
+        int i = 0;
+
+        public Page2()
+        {
+            InitializeComponent();
+
+            BuildAds();
+
+            dt.Interval = new TimeSpan(0, 0, 0, 0, 5000); // 500 Milliseconds
+            dt.Tick += new EventHandler(dt_Tick);
+            dt.Start();
+        }
+
+        void dt_Tick(object sender, EventArgs e)
+        {
+
+            i %= 3;
+            Title1.Text = ads[i].title;
+            Phone1.Text = ads[i].phone;
+            Location1.Text = ads[i].location;
+            BitmapImage tn = new BitmapImage();
+            tn.SetSource(Application.GetResourceStream(new Uri(ads[i].image, UriKind.Relative)).Stream);
+            Image1.Source = tn;
+
+            Title1.Visibility = System.Windows.Visibility.Visible;
+            Phone1.Visibility = System.Windows.Visibility.Visible;
+            Delete1.Visibility = System.Windows.Visibility.Visible;
+            DeleteImage1.Visibility = System.Windows.Visibility.Visible;
+            Location1.Visibility = System.Windows.Visibility.Visible;
+            Image1.Visibility = System.Windows.Visibility.Visible;
+
+            i++;
+        }
+
+        private void BuildAds()
+        {
+            Title1.Visibility = System.Windows.Visibility.Collapsed;
+            Phone1.Visibility = System.Windows.Visibility.Collapsed;
+            Delete1.Visibility = System.Windows.Visibility.Collapsed;
+            DeleteImage1.Visibility = System.Windows.Visibility.Collapsed;
+            Location1.Visibility = System.Windows.Visibility.Collapsed;
+            Image1.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Title1.Visibility = System.Windows.Visibility.Collapsed;
+            Phone1.Visibility = System.Windows.Visibility.Collapsed;
+            Delete1.Visibility = System.Windows.Visibility.Collapsed;
+            DeleteImage1.Visibility = System.Windows.Visibility.Collapsed;
+            Location1.Visibility = System.Windows.Visibility.Collapsed;
+            Image1.Visibility = System.Windows.Visibility.Collapsed;
+
+            dt.Stop();
+        }
+
+    }
+}
